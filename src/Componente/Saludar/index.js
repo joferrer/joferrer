5e3b6c9ad2b60d8c4a0de2cec891b15a2de48f38
jeison ferrer ortega <jeison120120@hotmail.com>
2019-10-07 19:45:52 -0500

    ADD de la barra head y la imagen

--- src/Componente/Saludar/index.js
+++ src/Componente/Saludar/index.js
@@ -2,7 +2,36 @@ import React,{Component} from 'react';
 class Saludar extends Component {
   render() {
     return(
-      <h1>Saludando...</h1>
+      <div>
+
+
+  <title>Bootstrap Example</title>
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <ul class="navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Active</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link disabled" href="#">Disabled</a>
+    </li>
+
+  </ul>
+
+  <form class="form-inline" action="/action_page.php">
+  <input class="form-control mr-sm-2" type="text" placeholder="Search"></input>
+  <button class="btn btn-success" type="submit">Search</button>
+  </form>
+  </nav>
+
+    <h1>PAPAA!!</h1>
+    <img src={'https://aka.weightwatchers.com/images/1033/dynamic/GCMSImages/Potato_main.jpg'} />
+    </div>
     );
   }
 }
